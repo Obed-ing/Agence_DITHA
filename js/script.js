350cf00dc2e5d321d782b1a41cb18cc99d301053
Edith Diarra <diarraedith234@gmail.com>
2022-04-22 16:31:53 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// let img_click = document.getElementsByClassName("eo");
+// let txt = document.getElementsByClassName("eo");
+
+
+function d_text(){
+    //  this.classList.toggle("out")
+     alert(this.value)
+}
+
